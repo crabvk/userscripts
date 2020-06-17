3ec7867daac9d9c1f3375c1e47d038963f694ce6
Vyacheslav Konovalov <vyachkonovalov@protonmail.com>
2020-06-17 11:21:11 +0500

    restore position on track change

--- soundcloud-restore-playback.user.js
+++ soundcloud-restore-playback.user.js
@@ -2,28 +2,23 @@
 // @name        SoundCloud Restore Playback
 // @namespace   https://github.com/vyachkonovalov
 // @description Saves/restores playback position on SoundCloud.com
-// @version     0.1.0
+// @version     0.2.0
 // @author      Vyacheslav Konovalov
 // @match       https://soundcloud.com/*
 // @license     MIT
 // @noframes
 // @grant       GM_getValue
 // @grant       GM_setValue
+// @grant       GM_deleteValue
 // @downloadURL https://raw.githubusercontent.com/vyachkonovalov/userscripts/master/soundcloud-restore-playback.user.js
 // @supportURL  https://t.me/vyachkonovalov
 // @homepageURL https://github.com/vyachkonovalov/vyachkonovalov/userscripts
 // ==/UserScript==
 
-// Store last saved playback position, mutation.oldValue is always null for unknown reason
+let lastKey
 let lastPosition
 
-const restorePlayback = (key, wrapper) => {
-  const position = GM_getValue(key)
-  if (position === undefined) {
-    return
-  }
-  lastPosition = position
-
+const restorePlayback = (position, wrapper) => {
   const length = parseInt(wrapper.getAttribute('aria-valuemax'))
   const rect = wrapper.getBoundingClientRect()
   const args = {
@@ -39,22 +34,41 @@ const restorePlayback = (key, wrapper) => {
 const getKey = player => player.querySelector('.playbackSoundBadge__titleLink').getAttribute('href')
 
 const observeProgress = player => {
+  let isInit = true
   const wrapper = player.querySelector('.playbackTimeline__progressWrapper')
   new MutationObserver(mutations => {
     for (const mutation of mutations) {
       if (mutation.type === 'attributes') {
-        if (mutation.attributeName === 'aria-valuemax') {
-          restorePlayback(getKey(player), mutation.target)
-          break
-        } else if (mutation.attributeName === 'aria-valuenow' && typeof lastPosition === 'number') {
-          setTimeout(() => {
-            const position = parseInt(mutation.target.getAttribute('aria-valuenow'))
+        const isInitRestore = isInit && mutation.attributeName === 'aria-valuemax'
+        const isPositionUpdate = !isInitRestore && mutation.attributeName === 'aria-valuenow'
+
+        if (isInitRestore || isPositionUpdate) {
+          const key = getKey(player)
+          let position
+
+          if (isPositionUpdate && key === lastKey) {
+            position = parseInt(mutation.target.getAttribute('aria-valuenow'))
             if (position > 0 && position % 5 === 0 || Math.abs(lastPosition - position) > 4) {
-              // Save current position
-              lastPosition = position
               GM_setValue(getKey(player), position)
             }
-          }, 0)
+          } else {
+            position = GM_getValue(key) || 0
+            if (position > 0) {
+              const length = parseInt(mutation.target.getAttribute('aria-valuemax'))
+              // Do not restore position from last 5% of the track,
+              // but this 5% can't be lower than 10 seconds or greater than 30 seconds
+              const limit = length - Math.min(Math.max(length / 100 * 5, 10), 30)
+              if (position < limit) {
+                restorePlayback(position, mutation.target)
+              } else {
+                GM_deleteValue(key)
+              }
+            }
+            isInit = false
+          }
+
+          lastKey = key
+          lastPosition = position
           break
         }
       }
